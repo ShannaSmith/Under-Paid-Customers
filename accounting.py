fb93790df430eda1fe98fc65d37a1bb30f234d4d
ShannaSmith <shannagjh@gmail.com>
2022-11-03 19:28:39 -0400

    convert strings to int and floats

--- accounting.py
+++ accounting.py
@@ -26,7 +26,7 @@ customer6_paid = 2.00
 
 
 def customer_Payments(customers_orders_file):
-    melon_cost = int(1.00)
+    melon_cost = float(int(1.00))
     customers_orders = open(customers_orders_file)
     
     for line in customers_orders:
@@ -34,48 +34,13 @@ def customer_Payments(customers_orders_file):
         words = line.split('|')
 
         
-        customer = words[1]
-        count = words[2]
-        customer_payment= int(words[3])
+        customer_name = words[1]
+        count = int(words[2])
+        customer_payment= float(int(words[3]))
         customer_expected = melon_cost * count
         if customer_expected != customer_payment:
-            print(f"{customer} paid ${customer_payment: .2f},",
+            print(f"{customer_name} paid ${customer_payment: .2f},",
                 f"expected ${customer_expected: .2f}")
        
+    customers_orders_file.close()   
 
-
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print(f"{customer1_name} paid ${customer1_paid:.2f},",
-          f"expected ${customer1_expected:.2f}"
-          )
-
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print(f"{customer2_name} paid ${customer2_paid:.2f},",
-          f"expected ${customer2_expected:.2f}"
-          )
-
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print(f"{customer3_name} paid ${customer3_paid:.2f},",
-          f"expected ${customer3_expected:.2f}"
-          )
-
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print(f"{customer4_name} paid ${customer4_paid:.2f},",
-          f"expected ${customer4_expected:.2f}"
-          )
-
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print(f"{customer5_name} paid ${customer5_paid:.2f},",
-          f"expected ${customer5_expected:.2f}"
-          )
-
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print(f"{customer6_name} paid ${customer6_paid:.2f},",
-          f"expected ${customer6_expected:.2f}"
-          )
